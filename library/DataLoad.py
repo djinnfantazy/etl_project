295499d399992d2a89f3fca148f8439ad0015e69
Alexey Efimov <aaefimov1998@gmail.com>
2025-01-27 19:45:31 +0100

    remove comments and unused files

--- library/DataLoad.py
+++ library/DataLoad.py
@@ -8,20 +8,9 @@ import requests
 API_BASE = "https://newsapi.org/v2/top-headlines?country=us"
 api_key = os.environ.get("NEWS_API_KEY")
 
-# rawSchema = StructType([
-#         StructField("status", StringType()),
-#         StructField("totalResults", IntegerType()),
-#         StructField("articles", StringType())
-#     ])
-
 def df_to_file(spark, df):
     spark = DataFrameReader(spark)
     df.write.mode("overwrite").json('landing_zone')
-
-# def raw_data_to_df(spark, path):
-#     spark = SparkSession(spark)
-#     df = spark.read.json(path = path, schema = rawSchema)
-#     return df
 
 def api_resp_to_df(spark, sparkcontext):
     spark = SparkSession(spark)
