04ab3a8c326bd202058bffc5f7bbd159c7b29733
Alexey Efimov <aaefimov1998@gmail.com>
2025-02-03 13:26:26 +0100

    remove unnecessary abstract class and methods, update error handling and add docstrings

--- library/api_extract.py
+++ library/api_extract.py
@@ -1,45 +1,41 @@
 import os
 import requests
-from abc import ABC, abstractmethod
-class Extract(ABC):
-    def __init__(self, api_base : str, key_name : str):
-        self.api_base = api_base
-        self.key_name = key_name
+class NewsApiExtract():
 
     def get_api_key(self) -> str:
-        api_key = os.environ.get(self.key_name)
+        """
+        Retrieve the News API key 
+        from the environment variable.
+        """
+        api_key = os.environ.get("NEWS_API_KEY")
         if not api_key:
-            raise ValueError(f"The API key {self.key_name}is missing, please set it as an environment variable.")
+            raise ValueError(f"The 'NEWS_API_KEY' environment variable is missing, make sure it is set and try again.")
         return api_key
     
     def get_url_base(self) -> str:
-        """Get the url base"""
+        """
+        Get the url base.
+        """
 
-        return self.api_base
+        return "https://newsapi.org/v2/"
     
     def get_header(self, api_key : str) -> dict[str, str]:
+        """
+        Get the header required for the API request.
+        """
         return {'X-Api-Key' : api_key}
     
-    def make_request(self, endpoint : str, country : str | None) -> dict:
-        if type(country) is str:
-            url = f"{self.api_base}{endpoint}?country={country}"
-        else:
-            url = f'{self.api_base}{endpoint}'
+    def get_data(self, endpoint : str = 'top-headlines', country : str = 'us') -> dict[str, str]:
+        """
+        Retrieve top headlines from News API
+        by the 'articles' key as a JSON object.
+        """
+        url_base = self.get_url_base()
+        url = f"{url_base}{endpoint}?country={country}"
         api_key = self.get_api_key()
         headers = self.get_header(api_key)
         response = requests.get(url, headers=headers)
-        if response.status_code != 200:
-            raise requests.exceptions.HTTPError('API request failed.')
-        return response.json()
-
-    @abstractmethod
-    def extract(self, **kwargs) -> dict:
-        pass
-
-class NewsApiExtract(Extract):
-
-    def __init__(self):
-        super().__init__(api_base='https://newsapi.org/v2/', key_name='NEWS_API_KEY')
-
-    def extract(self, endpoint = 'top-headlines', country = 'us') -> dict:
-        return self.make_request(endpoint, country)
+        if response.json()["status"] == "error":
+            raise requests.exceptions.HTTPError(response["message"])
+        articles = response.json()['articles']
+        return articles
