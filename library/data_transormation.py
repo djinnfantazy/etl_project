222d8827578bc94281e52d2ccda8a0e55db791ac
Alexey Efimov <aaefimov1998@gmail.com>
2025-02-03 13:27:43 +0100

    remove unnecessary abstract class and methods, update the function generating df to use list of strings as input

--- library/data_transormation.py
+++ library/data_transormation.py
@@ -1,22 +1,27 @@
-from pyspark.sql import SparkSession, DataFrameReader
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import StructType, StringType
 from abc import ABC, abstractmethod
+import json
 
-class LandingTransform(ABC):
-    def __init__(self, 
-                spark : SparkSession, 
-                sparkcontext : SparkSession.sparkContext):
-        
+class NewsApiLandingTransform():
+    def __init__(self, spark : SparkSession):
         self.spark = spark
-        self.sparkcontext = sparkcontext
-    
-    @abstractmethod
-    def resp_to_df(self, **kwargs) -> DataFrameReader:
-        pass
 
-class NewsApiLandingTransform(LandingTransform):
+    def json_to_strings(self, json_obj) -> list[str]:
+        """
+        Tranfsorm the API JSON response to a list of strings.
+        """
 
-    def resp_to_df(self, api_resp) -> DataFrameReader:
-        articles = api_resp['articles']
-        rdd = self.sparkcontext.parallelize(articles)
-        df = self.spark.createDataFrame(rdd)
+        list_of_str = []
+        for item in json_obj:
+            list_of_str.append(json.loads(json.dumps(item)))
+        return list_of_str
+
+    def resp_to_df(self, spark : SparkSession, api_resp : list) -> DataFrame:
+        """
+        Transform the list of strings to a one-column dataframe.
+        """
+        schema = StructType().add("articles", StringType(), True)
+        rdd = spark.sparkContext.parallelize(api_resp)
+        df = spark.createDataFrame(rdd.map(lambda x: (x,)), schema=schema)
         return df
