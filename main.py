f0e1e39c1c2ba67605099648d4f89aa9415e2ad8
Alexey Efimov <aaefimov1998@gmail.com>
2025-03-10 18:02:50 +0100

    update main file to use udpated classes and remove unnecessary configs

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from pyspark.sql.session import SparkSession
 from library.logger import Log4j
 from library.api_extract import NewsApiExtract
 from library.ingest.ingest import IngestConfig
+from library.config_utils import ConfigUtils
 from library.utils import DictUtils
 import os
 
-landing_zone = "landing_zone"
 SOURCE_NAME = "news_api"
 DATASET = "top-headlines"
-CONFIG_PATH = os.path.join(os.path.abspath(os.curdir), 'resources', 'news_api', 'ingest.yml')
 
 from delta.tables import *
 from delta import configure_spark_with_delta_pip
@@ -27,8 +26,7 @@ spark = configure_spark_with_delta_pip(spark, ["io.delta:delta-core_2.12:2.4.0",
 spark.sparkContext.addPyFile("C:\\spark-3.4.4\\jars\\delta-storage-2.4.0.jar")
 
 logger = Log4j(spark)
-
-ingest_config = IngestConfig(**DictUtils.read_config(CONFIG_PATH, DATASET))
+ingest_config = IngestConfig(**ConfigUtils.get_ingest_config(SOURCE_NAME)[DATASET])
 
 logger.info("Retrieving data from the API")
 rest_api_data = NewsApiExtract().get_data(DATASET)
@@ -36,7 +34,7 @@ rest_api_data = NewsApiExtract().get_data(DATASET)
 logger.info("Saving the data to a df")
 df = spark.createDataFrame(
     DictUtils.get_by_path(rest_api_data, ingest_config.json_data_path),
-    DictUtils.get_struct_schema(ingest_config.schema)
+    IngestConfig.get_struct_schema(ingest_config.schema)
 )
 df.show(10)
 
