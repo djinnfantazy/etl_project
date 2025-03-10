79985bb99980070b70d675a109dfef52feb43ca3
Alexey Efimov <aaefimov1998@gmail.com>
2025-03-10 19:21:55 +0100

    refactor main.py to improve Spark session configuration and data ingestion process

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pyspark.sql.session import SparkSession
+from pyspark.pandas import read_delta
+from pyspark.sql.functions import current_date
 from library.logger import Log4j
 from library.api_extract import NewsApiExtract
-from library.ingest.ingest import IngestConfig
+from library.ingest.ingest import IngestConfig, Ingest
 from library.config_utils import ConfigUtils
 from library.utils import DictUtils
 import os
@@ -12,18 +14,30 @@ DATASET = "top-headlines"
 from delta.tables import *
 from delta import configure_spark_with_delta_pip
 
-spark = (
+# spark = (
+#     SparkSession
+#     .builder
+#     .appName("NewsAPIProject")
+#     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+#     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+#     .config("spark.master", "local[3]"))
+
+
+# spark = configure_spark_with_delta_pip(spark, ["io.delta:delta-core_2.12:2.4.0", 
+#                                                "io.delta:delta-storage:2.4.0"]).getOrCreate()
+
+# spark.sparkContext.addPyFile("C:\\spark-3.4.4\\jars\\delta-storage-2.4.0.jar")
+
+
+spark_builder = (
     SparkSession
     .builder
-    .appName("NewsAPIProject")
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-    .config("spark.master", "local[3]"))
+    .config("spark.master", "local[3]")
+    )
 
-spark = configure_spark_with_delta_pip(spark, ["io.delta:delta-core_2.12:2.4.0", 
-                                               "io.delta:delta-storage:2.4.0"]).getOrCreate()
-
-spark.sparkContext.addPyFile("C:\\spark-3.4.4\\jars\\delta-storage-2.4.0.jar")
+spark = configure_spark_with_delta_pip(spark_builder).getOrCreate()
 
 logger = Log4j(spark)
 ingest_config = IngestConfig(**ConfigUtils.get_ingest_config(SOURCE_NAME)[DATASET])
@@ -32,13 +46,18 @@ logger.info("Retrieving data from the API")
 rest_api_data = NewsApiExtract().get_data(DATASET)
 
 logger.info("Saving the data to a df")
-df = spark.createDataFrame(
+df = (
+    spark.createDataFrame(
     DictUtils.get_by_path(rest_api_data, ingest_config.json_data_path),
-    IngestConfig.get_struct_schema(ingest_config.schema)
-)
+    IngestConfig.get_struct_schema(ingest_config.schema))
+    .withColumn("_createdOn", current_date())
+    )
 df.show(10)
 
-# Ingest().overwrite_delta(df = df)
+Ingest().overwrite_delta(df = df, partitionBy="publishedAt")
+
+d = spark.read.format("delta").load("landing_zone")
+d.show(10)
 
 logger.info("Finishing the NewsProject")
 
