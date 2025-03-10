6c982cd190d46af0480b91ae59985d006dd339c1
Alexey Efimov <aaefimov1998@gmail.com>
2025-03-10 18:04:13 +0100

    move methods from DictUtils to other classes

--- library/utils.py
+++ library/utils.py
@@ -5,12 +5,6 @@ from pyspark.sql.types import _parse_datatype_string, StructType
 class DictUtils:
 
     @staticmethod
-    def read_config(config_path, endpoint : str):
-        ''' Reads ingest configs for a particular endpoint  '''
-        conf_dict = yaml.safe_load(open(config_path))[endpoint]
-        return conf_dict
-
-    @staticmethod
     def get_by_path(d: dict, path: Optional[list[str]] = None) -> Any:
         ''' Parses REST API's output by a json_path key in the YAML config file'''
         if path:
@@ -21,8 +15,3 @@ class DictUtils:
 
         return d
     
-    @staticmethod
-    def get_struct_schema(schema : str) -> StructType:
-        ''' Parses a schema from the config file to be later used in dataframe creation'''
-        schema_str = _parse_datatype_string(schema)
-        return schema_str
